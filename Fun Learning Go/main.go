57a8778099f3592bfb6588b128a249b5927ec7d2
zuoyun <18787217093@163.com>
2025-07-17 21:09:45 +0800

    7.17新增今日进度

---
+++ Fun Learning Go/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"myGo/pack"
+)
+
+func main() {
+	// pack.Malacandra()
+	// pack.Guess(55) // 这里可以传入[1-100]之间的数字
+	// pack.Random_dates()
+	// pack.Ticket()
+	// pack.Piggy()
+	pack.Cains() // 储蓄模拟
+}
